f39a57fb9b819e805f1db27d6efca56c47537b36
Ishwar Soni <ishwar.soni@upgrad.com>
2020-06-14 21:19:14 +0530

    added order repository

--- src/main/java/com/upgrad/tacocloud/controllers/OrderController.java
+++ src/main/java/com/upgrad/tacocloud/controllers/OrderController.java
@@ -2,35 +2,49 @@ package com.upgrad.tacocloud.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.upgrad.tacocloud.entities.Order;
+import com.upgrad.tacocloud.repositories.OrderRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+	
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	public OrderController(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
+	
 	@GetMapping("/current")
 	public String orderForm (Model model) {
 		log.info("presenting order form");
-		model.addAttribute("order", new Order());
 		return "orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder (@Valid Order order, Errors errors) {
+	public String processOrder (@Valid Order order, Errors errors, SessionStatus sessionStatus) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
 		
 		log.info("processing order details : " + order);
+		orderRepository.save(order);
+		sessionStatus.setComplete();
 		return "redirect:/";
 	}
 }
